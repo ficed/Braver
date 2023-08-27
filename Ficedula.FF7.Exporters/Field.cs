43e63a4808ba5be2a7635ed641104f24dd208423
ficedula <none@none>
2023-08-27 10:40:19 +0100

    Added missing project - d'oh!

---
+++ Ficedula.FF7.Exporters/Field.cs
@@ -0,0 +1,43 @@
+﻿namespace Ficedula.FF7.Exporters {
+
+    public class FieldLayerState {
+        public SkiaSharp.SKBitmap Bitmap { get; init; }
+        public int Layer { get; set; }
+        public int Key { get; set; }
+    }
+    public static class FieldExport {
+        public static IEnumerable<FieldLayerState> Export(this Field.Background background) {
+            int offsetX = -background.AllSprites.Min(s => s.DestX),
+                offsetY = -background.AllSprites.Min(s => s.DestY);
+
+            int L = 0;
+            foreach (var layer in new[] { background.Layer1, background.Layer2 }) {
+                foreach (var tiles in layer.GroupBy(s => s.Blending)) {
+                    var bmp = new SkiaSharp.SKBitmap(background.Width, background.Height, SkiaSharp.SKColorType.Rgba8888, SkiaSharp.SKAlphaType.Premul);
+                    foreach (var tile in tiles.OrderBy(t => t.ID)) {
+                        int destX = tile.DestX + offsetX, destY = tile.DestY + offsetY;
+                        var src = background.Pages[tile.TextureID].Data;
+                        var pal = background.Palettes[tile.PaletteID].Colours;
+                        foreach (int y in Enumerable.Range(0, 16)) {
+                            foreach (int x in Enumerable.Range(0, 16)) {
+                                byte p = src[tile.SrcY + y][tile.SrcX + x];
+                                //                        if (p != 0)
+                                uint c = pal[p];
+                                c = (c & 0xff00ff00) | ((c & 0xff) << 16) | ((c & 0xff0000) >> 16);
+                                if ((c >> 24) != 0)
+                                    bmp.SetPixel(destX + x, destY + y, new SkiaSharp.SKColor(c));
+                            }
+                        }
+                    }
+
+                    yield return new FieldLayerState {
+                        Bitmap = bmp,
+                        Layer = L,
+                        Key = tiles.Key,
+                    };
+                }
+                L++;
+            }
+        }
+    }
+}
---
+++ Ficedula.FF7.Exporters/TexFile.cs
@@ -0,0 +1,24 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ficedula.FF7.Exporters {
+    public static class TexFileUtil {
+        public unsafe static SKBitmap ToBitmap(this TexFile tex, int palette) {
+            var data = tex.ApplyPalette(palette);
+            var bmp = new SKBitmap(tex.Width, tex.Height, SKColorType.Rgba8888, SKAlphaType.Premul);
+            foreach(int y in Enumerable.Range(0, tex.Height)) {
+                var row = data[y];
+                IntPtr pRow = bmp.GetAddress(0, y);
+                foreach (int x in Enumerable.Range(0, tex.Width))
+                    Marshal.WriteInt32(pRow, x * 4, (int)row[x]);
+            }
+
+            return bmp;
+        }
+    }
+}
