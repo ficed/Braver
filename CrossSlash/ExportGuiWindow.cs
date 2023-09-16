65275487d4b5a5fd0aeb14c7f541132ce3badc59
ficedula <none@none>
2023-09-16 17:09:26 +0100

    Add CrossSlash command line exporter program

---
+++ CrossSlash/ExportGuiWindow.cs
@@ -0,0 +1,146 @@
+﻿// This program and the accompanying materials are made available under the terms of the
+//  Eclipse Public License v2.0 which accompanies this distribution, and is available at
+//  https://www.eclipse.org/legal/epl-v20.html
+//  
+//  SPDX-License-Identifier: EPL-2.0
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terminal.Gui;
+
+namespace CrossSlash {
+    public class ExportGuiWindow : Window {
+
+        private Label _lblLGP, _lblGLB;
+        private ListView _lvHRCs;
+        private TextView _txtAnims;
+        private List<string> _hrcFiles;
+
+        private string _lgpFile, _glbFile;
+
+        public ExportGuiWindow() {
+            Title = "CrossSlash Exporter (Ctrl-Q to Quit)";
+
+            var btnLGP = new Button {
+                Text = "Select char LGP file",
+                Width = Dim.Percent(25),
+            };
+            btnLGP.Clicked += BtnLGP_Clicked;
+
+            _lblLGP = new Label {
+                Text = "(No LGP selected)",
+                X = Pos.Right(btnLGP) + 1,
+            };
+
+            Label lblHRC = new Label {
+                Y = Pos.Bottom(btnLGP) + 1,
+                Width = Dim.Percent(25),
+                Text = "HRC/Model",
+            };
+
+            _lvHRCs = new ListView {
+                Y = lblHRC.Y,
+                X = Pos.Right(lblHRC),
+                Width = Dim.Fill(1),
+                Height = 8,
+            };
+
+            Label lblAnims = new Label {
+                Width = Dim.Percent(25),
+                Y = Pos.Bottom(_lvHRCs) + 1,
+                Text = "Animations:"
+            };
+
+            _txtAnims = new TextView {
+                X = Pos.Right(lblAnims) + 1,
+                Y = lblAnims.Y,
+                Width = Dim.Fill(),
+                Height = 8,
+            };
+
+            var btnGLB = new Button {
+                Text = "Save GLB As",
+                Width = Dim.Percent(25),
+                Y = Pos.Bottom(_txtAnims) + 1,
+            };
+            btnGLB.Clicked += BtnGLB_Clicked;
+
+            _lblGLB = new Label {
+                X = Pos.Right(btnGLB) + 1,
+                Y = btnGLB.Y,
+                Text = "(No file selected)",
+            };
+
+            var btnExport = new Button {
+                Y = Pos.Bottom(btnGLB) + 1,
+                Width = Dim.Fill(1),
+                Text = "Export"
+            };
+            btnExport.Clicked += BtnExport_Clicked;
+
+            Add(btnLGP, _lblLGP, lblHRC, _lvHRCs, lblAnims, _txtAnims, btnGLB, _lblGLB, btnExport);
+        }
+
+        private void BtnExport_Clicked() {
+            try {
+                var anims = _txtAnims.Text
+                    .ToString()
+                    .Split('\r', '\n')
+                    .Where(s => !string.IsNullOrWhiteSpace(s));
+
+                if (!File.Exists(_lgpFile))
+                    throw new Exception("No LGP file selected");
+                if (string.IsNullOrEmpty(_glbFile))
+                    throw new Exception("No GLB save as filename selected");
+                if (_lvHRCs.SelectedItem < 0)
+                    throw new Exception("No HRC file selected");
+                if (!anims.Any())
+                    throw new Exception("No animations specified");
+
+                using (var lgp = new Ficedula.FF7.LGPFile(_lgpFile)) {
+                    var exporter = new Ficedula.FF7.Exporters.FieldModel(lgp);
+                    var model = exporter.BuildScene(_hrcFiles[_lvHRCs.SelectedItem], anims);
+                    model.SaveGLB(_glbFile);
+                }
+                MessageBox.Query("Success", "Export Succeeded", "OK");
+            } catch (Exception ex) {
+                MessageBox.ErrorQuery("Error", ex.Message, "OK");
+            }
+        }
+
+        private void BtnGLB_Clicked() {
+            var d = new SaveDialog(
+                "Save As", "Save output model to which file",
+                new List<string> { ".glb" }
+            );
+            Application.Run(d);
+            if (!d.Canceled && d.FileName != null) {
+                _lblGLB.Text = _glbFile = (string)d.FilePath;
+            }
+        }
+
+        private void BtnLGP_Clicked() {
+            var d = new OpenDialog(
+                "Open LGP", "Choose the char.lgp file to read models from",
+                new List<string> { ".lgp" }
+            );
+            Application.Run(d);
+            if (!d.Canceled && d.FilePaths.Any()) {
+                try {
+                    using (var lgp = new Ficedula.FF7.LGPFile(d.FilePaths[0])) {
+                        _hrcFiles = lgp.Filenames
+                            .Where(s => Path.GetExtension(s).Equals(".hrc", StringComparison.InvariantCultureIgnoreCase))
+                            .ToList();
+                        _lvHRCs.SetSource(_hrcFiles);
+                    }
+                } catch (Exception ex) {
+                    MessageBox.ErrorQuery("Error", ex.Message, "OK");
+                }
+                _lblLGP.Text = _lgpFile = d.FilePaths[0];
+            }
+        }
+    }
+}
---
+++ CrossSlash/Program.cs
@@ -0,0 +1,34 @@
+﻿// This program and the accompanying materials are made available under the terms of the
+//  Eclipse Public License v2.0 which accompanies this distribution, and is available at
+//  https://www.eclipse.org/legal/epl-v20.html
+//  
+//  SPDX-License-Identifier: EPL-2.0
+
+using CrossSlash;
+using Terminal.Gui;
+
+Console.WriteLine("CrossSlash");
+
+switch (args.Length) {
+    case 0:
+        Application.Run<ExportGuiWindow>();
+        Application.Shutdown();
+        break;  
+    case 1:
+    case 2:
+    case 3:
+        Console.WriteLine("USAGE: CrossSlash [OutputGLBFile] [LGPFile] [HRCFile] [Anim] [Anim] [Anim]...");
+        Console.WriteLine(@"e.g. CrossSlash C:\temp\tifa.glb C:\games\FF7\data\field\char.lgp AAGB.HRC ABCD.a ABCE.a");
+        break;
+    default:
+        Console.WriteLine($"Opening LGP {args[1]}...");
+        using (var lgp = new Ficedula.FF7.LGPFile(args[1])) {
+            var exporter = new Ficedula.FF7.Exporters.FieldModel(lgp);
+            Console.WriteLine($"Exporting model {args[2]}...");
+            var model = exporter.BuildScene(args[2], args.Skip(3));
+            Console.WriteLine($"Saving output to {args[0]}...");
+            model.SaveGLB(args[0]);
+            Console.WriteLine("Done");
+        }
+        break;
+}
